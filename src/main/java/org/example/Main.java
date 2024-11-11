f0fa50e07a0a876070efdbd723c473e2ecb54c2f
liuxiaobo <1294605662@qq.com>
2024-11-11 16:54:37 +0800

    RM ID: 2024/11/11

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,7 +16,7 @@ public class Main  {
 
     public static void main(String[] args) {
         //aa
-
+            //aas
         ApplicationContext context = SpringApplication.run(Main.class, args);
 
 //        B proxy = (B) context.getBean(B.class);
